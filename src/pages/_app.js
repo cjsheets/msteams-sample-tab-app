4201ebf68c5dcfd9ef9857122bdb94876cab2ff8
Chad Sheets <cjsheets@gmail.com>
2020-09-09 19:27:42 -0700

    initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,49 @@
+import '../styles/globals.css';
+import { createTheme, Customizer, Fabric, initializeIcons } from 'office-ui-fabric-react';
+import Head from 'next/head';
+
+initializeIcons();
+const theme = createTheme({
+  palette: {
+    themePrimary: '#00592b',
+    themeLighterAlt: '#cfebdd',
+    themeLighter: '#a7d9bf',
+    themeLight: '#83c7a4',
+    themeTertiary: '#63b48a',
+    themeSecondary: '#47a273',
+    themeDarkAlt: '#30905e',
+    themeDark: '#1c7e4b',
+    themeDarker: '#0c6b3a',
+    neutralLighterAlt: '#eeeeee',
+    neutralLighter: '#eaeaea',
+    neutralLight: '#e1e1e1',
+    neutralQuaternaryAlt: '#d1d1d1',
+    neutralQuaternary: '#c8c8c8',
+    neutralTertiaryAlt: '#c0c0c0',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#f4f4f4',
+  },
+});
+
+// (expected) Warning: Prop `className` did not match.
+// https://github.com/microsoft/fluentui/wiki/Server-side-rendering-and-browserless-testing
+function App({ Component, pageProps }) {
+  return (
+    <Customizer settings={{ theme }}>
+      <Fabric applyTheme>
+        <Head>
+          <title>Sample Tab App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </Fabric>
+    </Customizer>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import styles from '../styles/pages-index.module.css';
+
+export default function Index() {
+  return <div className={styles.container}>Welcome to the sample tab app</div>;
+}
